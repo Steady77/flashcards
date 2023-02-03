a57d973835739abcf21c9f540ebf9c8315d1b139
Surov Denis <90111915+Steady77@users.noreply.github.com>
2023-02-03 15:38:06 +0300

    refact: rename user table

--- server/prisma/migrations/20230203122946_create_user_table/migration.sql
+++ server/prisma/migrations/20230203122946_create_user_table/migration.sql
@@ -1,5 +1,5 @@
 -- CreateTable
-CREATE TABLE "users" (
+CREATE TABLE "User" (
     "id" SERIAL NOT NULL,
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMP(3) NOT NULL,
@@ -8,8 +8,8 @@ CREATE TABLE "users" (
     "password" TEXT NOT NULL,
     "image" TEXT NOT NULL,
 
-    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
-CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
