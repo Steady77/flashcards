80628b65f2bb4d39366f408766c5cb06f418c57d
Surov Denis <90111915+Steady77@users.noreply.github.com>
2023-02-11 16:47:56 +0300

    feat: add delete and update functions for deck

--- server/src/deck/deck.routes.ts
+++ server/src/deck/deck.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 import { checkAuth } from '../middleware/auth.middleware';
-import { createDeck, getAllDecks } from './deck.controller';
+import { createDeck, deleteDeck, getAllDecks, updateDeck } from './deck.controller';
 
 const router = Router();
 
 router.route('/').post(checkAuth, createDeck).get(checkAuth, getAllDecks);
+router.route('/:deckId').delete(checkAuth, deleteDeck).put(checkAuth, updateDeck);
 
 export default router;
