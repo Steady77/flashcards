e5fa36c6b7149f9288c2770f434288877d2447dc
Surov Denis <90111915+Steady77@users.noreply.github.com>
2023-02-11 16:43:50 +0300

    feat: add delete and update functions for deck

--- server/src/deck/deck.controller.ts
+++ server/src/deck/deck.controller.ts
@@ -4,32 +4,98 @@ import { prisma } from '../prisma';
 import { DeckBody, TypedRequestBody } from '../types/express/request.interface';
 
 export const createDeck = asyncHandler(async (req: TypedRequestBody<DeckBody>, res: Response) => {
-	const { title } = req.body;
-	const id = req.user?.id;
+	try {
+		const { title } = req.body;
+		const userId = req.user?.id;
 
-	if (id) {
+		if (!userId) return;
+
 		const deck = await prisma.deck.create({
 			data: {
 				title,
-				userId: id,
+				user: {
+					connect: {
+						id: userId,
+					},
+				},
 			},
 		});
 
 		res.json(deck);
+	} catch (error: any) {
+		console.log(error.message);
+		res.status(500).json({
+			message: 'Internal Server Error',
+		});
+	}
+});
+
+export const deleteDeck = asyncHandler(async (req: Request, res: Response) => {
+	try {
+		const deckId = req.params.deckId;
+
+		const deck = await prisma.deck.delete({
+			where: {
+				id: Number(deckId),
+			},
+			include: {
+				cards: true,
+			},
+		});
+
+		res.json(deck);
+	} catch (error) {
+		res.status(500).json({
+			message: 'Something went wrong',
+		});
+	}
+});
+
+export const updateDeck = asyncHandler(async (req: TypedRequestBody<DeckBody>, res: Response) => {
+	try {
+		const { title } = req.body;
+		const deckId = req.params.deckId;
+
+		const deck = await prisma.deck.update({
+			where: {
+				id: Number(deckId),
+			},
+			data: {
+				title: title,
+			},
+			include: {
+				cards: true,
+			},
+		});
+
+		res.json(deck);
+	} catch (error) {
+		res.status(500).json({
+			message: 'Something went wrong',
+		});
 	}
 });
 
 export const getAllDecks = asyncHandler(async (req: Request, res: Response) => {
-	const id = req.user?.id;
+	try {
+		const id = req.user?.id;
 
-	const decks = await prisma.deck.findMany({
-		where: {
-			userId: id,
-		},
-		include: {
-			cards: true,
-		},
-	});
+		const decks = await prisma.deck.findMany({
+			orderBy: {
+				createdAt: 'desc',
+			},
+			where: {
+				userId: id,
+			},
+			include: {
+				cards: true,
+			},
+		});
 
-	res.json(decks);
+		res.json(decks);
+	} catch (error) {
+		res.status(500).json({
+			message: 'Something went wrong',
+		});
+	}
 });
